c8862e638e6297663f604ba18effe7fc01f3f86f
Shiver211 <2863677073@qq.com>
2026-09-13 16:52:08 +0800

    ？怎么漏了一个文件

---
+++ src/main/java/com/shiver/polymorphlegacy/client/CraftingGuiOrigin.java
@@ -0,0 +1,8 @@
+package com.shiver.polymorphlegacy.client;
+
+/** 多面板界面提供合成槽所在主面板的原点。 */
+public interface CraftingGuiOrigin {
+    int polymorph$getCraftingLeft();
+
+    int polymorph$getCraftingTop();
+}
